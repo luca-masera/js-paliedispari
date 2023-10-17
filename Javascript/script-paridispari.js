aa8de6afe00baa607de0f8a7e6a19a8dead7bac3
luca-masera <lucamasera1995@gmail.com>
2023-10-17 21:10:28 +0200

    somma pari o dispari

--- Javascript/script-paridispari.js
+++ Javascript/script-paridispari.js
@@ -16,14 +16,32 @@ button.addEventListener('click', function(){
 
 
     function getRndInteger(min,max){
-        return Math.floor(Math.random() * (max - min + 1)) + min;      
-        
-        
+        return Math.floor(Math.random() * (max - min + 1)) + min;   
+                
     }
     let numeroComputer = getRndInteger (1,5)
     console.log(numeroComputer)
 
-   
+    
+  
     let somma = parseInt(numeroUtente) + parseInt(numeroComputer);
     console.log(somma);
+
+
+
+    
+    let scritta = ''; 
+    
+
+    if (somma % 2 === 0){
+        scritta = 'la somma è pari'
+    
+        console.log(scritta)
+        
+    } else if (somma % 2 !==0) {
+        scritta = 'La somma è dispari'
+        console.log(scritta)        
+    }
+
+
 })
