6fd30868ff9e7cd0ea6ce2bdbd79fbacebe84cdc
luca-masera <lucamasera1995@gmail.com>
2023-10-17 16:09:08 +0200

    if stringa

--- Javascript/script-palindroma.js
+++ Javascript/script-palindroma.js
@@ -3,13 +3,18 @@
 const btn = document.querySelector ('.btn-primary')
 let list =[];
 
-btn.addEventListener('click', function confrontaParola(confronta) {
+btn.addEventListener('click', function uguaglianza ( stringa) {
 
     let parolaUtente = document.getElementById('parola').value;
+    parolaUtente ='';
     
-    let stringa = parolaUtente.split('').reverse().join('')
+    let stringa = parolaUtente.split('').reverse().join('');
     console.log(stringa)
     
+    
+    /*if (stringa === parolaUtente.reverse()){
+        let write = 'è una parola palindroma '
+    }*/
 
 })
 
