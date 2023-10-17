e0939bf0d1e0efa528ea2b62f3bbd4bfaaf2529b
luca-masera <lucamasera1995@gmail.com>
2023-10-17 15:17:37 +0200

    richiesta parola all'utente

---
+++ Javascript/script-palindroma.js
@@ -0,0 +1,9 @@
+
+
+const btn = document.querySelector ('.btn-primary')
+
+btn.addEventListener('click', function palindroma() {
+
+    let parolaUtente = document.getElementById('parola').value;
+    console.log (parolaUtente);
+})
